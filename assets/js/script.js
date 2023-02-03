7c912dc4d29b64c1b1ca4bb10c8758949dfef427
priit79 <priit.enno@gmail.com>
2023-02-03 16:47:56 +0200

    edit

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+alert("Hello, SDA"):
+
+console.log("I may be a Front-end developer");
+
+var fullName = "Priit Enno";
+
+console.log(fullName.length);
+
+let location = "Tallinn";
+
+const ZIPCODE = 742234;
+
+let isActive = true;
+let age = 32;
+let salary = 4500.98;
+
+//Comparsion
+let a = '5';
+let b = 5;
+
+if (a == b) {
+  console.log("A is same value as B");
+} else if(a === b ) {
+  console.log("A is same value as B"); // value and type comparsion
+}
