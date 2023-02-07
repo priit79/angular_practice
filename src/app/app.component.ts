17aa2989d8012e46be3efd8cc9f9379f76179e0a
priit79 <priit.enno@gmail.com>
2023-02-07 23:00:51 +0200

    vajuta nuppu ja ilmuvad koerte nimed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Dog} from "./dog/shared/models/Dog";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.doggies = this.fetchDoggos();
+  }
+
   title = 'SDA Front-End practice';
   toShow = true;
 
   fruits = ["apple", "banana", "Strawberry", "Pineapple"]
 
-  showAlertBox() {
-    alert("Click me is clicked");
+  doggies: Dog[] = [];
+  dogNames: string[] = ['Caesar', 'Scooby', 'Shadow'];
 
+  fetchDoggos(): Dog[] {
+    let doggos: Dog[] = [];
 
+    doggos.push(new Dog('Jack', 'Dober', 'Runs faster'));
+    doggos.push(new Dog('Julie', 'German Shepperd', 'Eats'));
 
-
+    return doggos;
   }
+
+  showAlertBox() {
+    alert("Woof! Woof!");
+  }
+
+  addDog() {
+    let dogName = this.dogNames.at(Math.floor(Math.random() * this.dogNames.length));
+    // @ts-ignore
+    this.doggies.push(new Dog(dogName, 'Dalmian', 'Barks louder'))
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DogComponent } from './dog/dog.component';
-import { DoggiesDirective } from './doggies.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     DogComponent,
-    DoggiesDirective
   ],
   imports: [
     BrowserModule
