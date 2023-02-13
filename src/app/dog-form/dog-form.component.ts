bf28b0e29c1ba1559fe32881d860ee1588a0d934
priit79 <priit.enno@gmail.com>
2023-02-13 21:28:46 +0200

    adding form to our application

---
+++ src/app/dog-form/dog-form.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {Dog} from "../dog/shared/models/Dog";
+import {NgForm} from "@angular/forms";
+
+@Component({
+  selector: 'app-dog-form',
+  templateUrl: './dog-form.component.html',
+  styleUrls: ['./dog-form.component.css']
+})
+export class DogFormComponent {
+
+  dogs: Dog[] = [];
+  name: string = '';
+  type: string = '';
+  property: string = '';
+
+  addDog(dogForm: NgForm) {
+    this.dogs.push(new Dog(this.name, this.type, this.property));
+    dogForm.reset();
+  }
+}
+
